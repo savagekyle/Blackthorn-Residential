ee5d46981400d0a2583d984cae49e65e49484e34
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-01-18 13:01:17 -0500

    styled navbar / began mobile responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
@@ -10,10 +17,23 @@ function Navbar() {
           <Link to="/" className="navbar-logo">
             BR
           </Link>
+          <FontAwesomeIcon
+            className="menu-icon"
+            onClick={handleClick}
+            icon={click ? faBars : faX}
+          />
           <ul className="list-items">
-            <li className="nav-links">Gallery</li>
-            <li className="nav-links">About</li>
-            <li className="nav-links">Contact</li>
+            <Link to="/" onClick={closeMobileMenu} className="nav-links">
+              <li>Gallery</li>
+            </Link>
+            <li className="nav-bars">|</li>
+            <Link to="/" onClick={closeMobileMenu} className="nav-links">
+              <li>About</li>
+            </Link>
+            <li className="nav-bars">|</li>
+            <Link to="/" onClick={closeMobileMenu} className="nav-links">
+              <li>Contact</li>
+            </Link>
           </ul>
         </div>
       </nav>
