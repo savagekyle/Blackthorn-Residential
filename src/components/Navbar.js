0fc269f67647ba5179cad8545d4d3a25f38b4aad
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-01-18 09:57:54 -0500

    initial navbar look

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,13 @@ function Navbar() {
       <nav className="navbar">
         <div className="container">
           <Link to="/" className="navbar-logo">
-            Test
+            BR
           </Link>
+          <ul className="list-items">
+            <li className="nav-links">Gallery</li>
+            <li className="nav-links">About</li>
+            <li className="nav-links">Contact</li>
+          </ul>
         </div>
       </nav>
     </>
