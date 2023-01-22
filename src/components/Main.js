3705ca0d29b652aa54a0ee11add235979e16b6a5
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-01-21 21:42:52 -0500

    updated landing page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./Main.css";
 
 function Main() {
-  return <div className="main-image">Main</div>;
+  return (
+    <div className="main-image">
+      <div className="plop">
+        <div className="br-title">Blackthorn</div>
+        <div className="br-title">Residential</div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
