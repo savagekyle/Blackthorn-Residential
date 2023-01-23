e086528133c89818bde8c1de4102ae346660d9d6
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-01-23 11:07:11 -0500

    implemented button component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./Main.css";
+import { Button } from "./Button";
+import { useState } from "react";
 
 function Main() {
+  const [button, setButton] = useState(true);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
       <div className="background"></div>
@@ -9,6 +23,26 @@ function Main() {
       <div className="br">
         <div className="br-title">Blackthorn</div>
         <div className="br-title">Residential</div>
+      </div>
+      <div className="btn-container">
+        {button && (
+          <Button
+            className="btn-spacing"
+            buttonSize="btn--large"
+            buttonStyle="btn--outline"
+          >
+            View Work
+          </Button>
+        )}
+        {button && (
+          <Button
+            className="btn-spacing"
+            buttonSize="btn--large"
+            buttonStyle="btn--primary"
+          >
+            Contact
+          </Button>
+        )}
       </div>
     </>
   );
