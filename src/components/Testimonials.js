117c60c143bf7121271f26c6ff330b1ce064cfa5
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-01-24 11:45:06 -0500

    main testimonial slider styles implemented

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,40 +1,54 @@
 import React from "react";
 import "./Testimonials.css";
-import Swiper from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/bundle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
 function Testimonials() {
-  const swiper = new Swiper(".swiper", {
-    pagination: {
-      el: ".swiper-pagination",
-    },
-
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
   return (
-    <div class="swiper">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        ...
-      </div>
-      <div class="swiper-pagination"></div>
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
-
-      <div class="swiper-scrollbar"></div>
-    </div>
+    <Swiper
+      modules={[Navigation, Pagination]}
+      spaceBetween={50}
+      navigation
+      pagination={{ clickable: true }}
+      loop={true}
+      autoplay
+      slidesPerView={1}
+    >
+      <SwiperSlide>
+        <div className="slide-container">
+          <h1>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </h1>
+          <FontAwesomeIcon className="quote-icon" icon={faQuoteLeft} />
+          <p>Kyle Savage</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="slide-container">
+          <h1>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </h1>
+          <FontAwesomeIcon className="quote-icon" icon={faQuoteLeft} />
+          <p>Rob Savage</p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="slide-container">
+          <h1>
+            My dad is simply the best builder in all of Knoxville. Easily.
+          </h1>
+          <FontAwesomeIcon className="quote-icon" icon={faQuoteLeft} />
+          <p>Olivia Savage</p>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
 
