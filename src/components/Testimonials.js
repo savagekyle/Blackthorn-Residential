81e257689516cece09646a57a308de20c733a2de
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-01-23 19:42:00 -0500

    Style changes to font

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,12 +1,39 @@
 import React from "react";
 import "./Testimonials.css";
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/bundle";
 
 function Testimonials() {
+  const swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
   return (
-    <div className="testimonial-container">
-      <div className="">
-        <div className="">Testimonials</div>
+    <div class="swiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+        ...
       </div>
+      <div class="swiper-pagination"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+
+      <div class="swiper-scrollbar"></div>
     </div>
   );
 }
