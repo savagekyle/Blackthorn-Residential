7a405675b2ba219928aef2b1f1be7f45fce75aa5
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-03-02 19:11:09 -0500

    minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
+  const date = new Date();
+  var year = date.getFullYear();
+
   return (
     <div className="footer-container">
       <footer>
-        Copyright &copy; 2023 Blackthorn Residential. All Rights Reserved.
+        Copyright &copy; {year} Blackthorn Residential. All Rights Reserved.
       </footer>
     </div>
   );
