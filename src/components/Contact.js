d34f270e9e56289ad2633da5b70386dafa97a84f
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-03-27 13:17:10 -0400

    polishing design and working on better mobile responsiveness

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,65 @@ import Logo from "../assets/full-blackthorn-logo-white.svg";
 function Contact() {
   return (
     <>
-      <div className="contact-image"></div>
+      <div className="contact">
+        <div className="contact-box">
+          <div className="general-info">
+            <div className="info">
+              <h1>GENERAL INFO</h1>
+              <hr></hr>
+              <p>Rob Savage</p>
+              <p>President</p>
+              <p>rob@blackthornresidential.com</p>
+              <p>(865) 388-7284</p>
+            </div>
+
+            <img src={Logo} alt="br logo" className="full-logo" />
+          </div>
+          <div className="contact-form">
+            <h1>GET IN CONTACT</h1>
+            <hr></hr>
+            <form
+              action="https://formsubmit.co/43bb4e4ef6ac96d45383ce660fad27aa"
+              method="POST"
+            >
+              <input type="hidden" name="_captcha" value="false"></input>
+              <input
+                type="text"
+                className="first-name-input"
+                name="name"
+                placeholder="Name"
+                required
+              />
+              <input
+                type="email"
+                className="email-input"
+                name="email"
+                placeholder="Email"
+                required
+              />
+              <input
+                type="text"
+                className="subject-input"
+                name="subject"
+                placeholder="Subject"
+              />
+              <textarea
+                className="message-input"
+                name="message"
+                placeholder="Type your message here ..."
+              ></textarea>
+              <Button type="submit">Send Message</Button>
+              <input
+                type="hidden"
+                name="_next"
+                value="http://localhost:3000/contact/thank-you"
+              ></input>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="contact-image"></div>
       <div className="form-position">
         <div className="form-container">
           <div className="general-info">
@@ -45,7 +103,7 @@ function Contact() {
             ></input>
           </form>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
