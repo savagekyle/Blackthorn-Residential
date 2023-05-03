5b8b850abededa36304d459466e91dad232596f0
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-05-03 17:18:35 -0400

    gallery page set up

--- src/components/galleryItems/GalleryItems.js
+++ src/components/galleryItems/GalleryItems.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./GalleryItems.css";
+import { Link } from "react-router-dom";
 
-const GalleryItems = () => {
-  return <div>GalleryItems</div>;
+const GalleryItems = (props) => {
+  let path = props.name.toLowerCase();
+  return (
+    <div className="gallery-item">
+      <Link to={path}>
+        <img className="gallery-thumbnail" src={props.img} alt={props.name} />
+        <h1>{props.name}</h1>
+      </Link>
+    </div>
+  );
 };
 
 export default GalleryItems;
--- src/scenes/gallery/galleryHome/Gallery.js
+++ src/scenes/gallery/galleryHome/Gallery.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Gallery.css";
+import GalleryItem from "../../../components/galleryItems/GalleryItems";
+import kitchenImg from "../../../assets/kitchen.HEIC";
+import exteriorImg from "../../../assets/exterior.HEIC";
+import bathroomImg from "../../../assets/bathroom.HEIC";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div className="gallery-home">
+      <GalleryItem img={kitchenImg} name="kitchens" />
+      <GalleryItem img={exteriorImg} name="exterior" />
+      <GalleryItem img={bathroomImg} name="bathrooms" />
+    </div>
+  );
 };
 
 export default Gallery;
