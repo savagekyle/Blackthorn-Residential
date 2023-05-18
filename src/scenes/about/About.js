4a7156f46475b9cb3ce7c2ee3dfca0aefd67d967
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-05-18 10:46:28 -0400

    Grammatical changes

--- src/scenes/about/About.js
+++ src/scenes/about/About.js
@@ -12,23 +12,23 @@ function About() {
             Founded in 2021 by Owner Rob Savage, Blackthorn Residential is a
             renovation and new home construction company that specializes in
             upper end renovations and additions. Having spent many years
-            building new homes and renovating countless others for top-tier
+            building new homes and renovating numerous others for top-tier
             builders, Rob decided to utilize his talents for design and
             innovation for himself and founded the company.
           </p>
           <p>
             Utilizing 3-D CAD for more than twenty years to estimate and
-            visualize each project under consideration, each project is
-            thoroughly analyzed for potential pitfalls and creative solutions to
-            give each client a specialized approach to their construction
-            projects. Seeing the project come to life in a model allows the
-            client to see and get excited about what their project might look
-            like when complete.
+            visualize every project being considered, each project is thoroughly
+            analyzed for potential pitfalls and creative solutions to give each
+            client a specialized approach to their construction projects. Seeing
+            the project come to life in a model allows clients to visualize and
+            get excited about what their project will look like when it's
+            complete.
           </p>
           <p>
             Whether you are looking to build a new home, renovate a kitchen or
-            bathroom, or add a new wing to your home, Blackthorn Residential is
-            here to make your stressful project stress-free.
+            bathroom, or add a new wing to your existing home, Blackthorn
+            Residential is here to make your stressful project stress-free.
           </p>
         </div>
         <div className="diagonal">
@@ -64,16 +64,16 @@ function About() {
             years in the Knoxville and Tellico Village markets. After many years
             of success, and the retirement of his father, Rob went to work for
             one of the premier builders in the market, working with and for the
-            most decerning customers, designers, and architects in the Knoxville
-            market. After many years of putting his talents to work for others,
-            Rob knew it was time to put his vast knowledge to work for himself
-            and his family.
+            most discerning customers, designers, and architects in the
+            Knoxville market. After many years of putting his talents to work
+            for others, Rob knew it was time to put his vast knowledge to work
+            for himself and his family.
           </p>
           <p>
             Today, Blackthorn Residential and Rob Savage have put their
-            attention to detail and quality construction to work creating
-            amazing additions and customer renovations for many in the Knoxville
-            Market.
+            attention to detail and focus on quality construction to work
+            creating amazing additions and customer renovations for many in the
+            Knoxville Market.
           </p>
           <hr />
           <p>
