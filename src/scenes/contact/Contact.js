81c64e2e7b1944350b0ebc8038f7d0373a72d417
savagekyle <66024623+savagekyle@users.noreply.github.com>
2023-05-18 14:07:48 -0400

    fixed compabitablity for production use

--- src/scenes/contact/Contact.js
+++ src/scenes/contact/Contact.js
@@ -22,7 +22,7 @@ function Contact() {
           <div className="contact-form">
             <h1>GET IN CONTACT</h1>
             <form
-              action="https://formsubmit.co/43bb4e4ef6ac96d45383ce660fad27aa"
+              action="https://formsubmit.co/rob@blackthornresidential.com"
               method="POST"
             >
               <input type="hidden" name="_captcha" value="false"></input>
@@ -55,53 +55,12 @@ function Contact() {
               <input
                 type="hidden"
                 name="_next"
-                value="http://localhost:3000/contact/thank-you"
+                value="https://blackthornresidential.com/contact/thank-you"
               ></input>
             </form>
           </div>
         </div>
       </div>
-
-      {/* <div className="contact-image"></div>
-      <div className="form-position">
-        <div className="form-container">
-          <div className="general-info">
-            <h1>GENERAL INFO</h1>
-            <hr></hr>
-            <p>Rob Savage</p>
-            <p>Owner</p>
-            <p>rob@blackthornresidential.com</p>
-            <p>(865) 388-7284</p>
-            <img src={Logo} alt="br logo" />
-          </div>
-          <form
-            className="contact-form"
-            action="https://formsubmit.co/43bb4e4ef6ac96d45383ce660fad27aa"
-            method="POST"
-          >
-            <input type="hidden" name="_captcha" value="false"></input>
-            <span className="first-name">Name</span>
-            <input
-              type="text"
-              className="first-name-input"
-              name="name"
-              required
-            />
-            <span className="email">Email</span>
-            <input type="email" className="email-input" name="email" required />
-            <span className="subject">Subject</span>
-            <input type="text" className="subject-input" name="subject" />
-            <span className="message">Message</span>
-            <textarea className="message-input" name="message"></textarea>
-            <Button type="submit">Send Message</Button>
-            <input
-              type="hidden"
-              name="_next"
-              value="http://localhost:3000/contact/thank-you"
-            ></input>
-          </form>
-        </div>
-      </div> */}
     </>
   );
 }
